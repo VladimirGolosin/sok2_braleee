f0d474d0233c3302742d2cabdb7694128c572918
stefanium1389 <83018844+stefanium1389@users.noreply.github.com>
2023-06-03 18:13:37 +0200

    joooj

--- code/FilePathParser/FilePathParser/kod/parse.py
+++ code/FilePathParser/FilePathParser/kod/parse.py
@@ -1,6 +1,7 @@
 from core.model.models import Node, Graph
 from core.services.parser import ParserService
 import os
+from datetime import datetime
 class FilePathParser(ParserService):
 
     def name(self):
@@ -17,18 +18,25 @@ class FilePathParser(ParserService):
         nodes = []
         edge_matrix = []
         def func(filepath):
+            n=0
+            for _, dirs, files in os.walk(path):
+                n += len(dirs)
+                n += len(files)
             for x in os.listdir(filepath):
                 curr_path = filepath + "\\" + x
+                attributes = {}
+                attributes["Size (bytes)"] = os.path.getsize(curr_path)
+                attributes["Last modified"] = datetime.fromtimestamp(os.path.getmtime(curr_path)).strftime(
+                    '%Y-%m-%d %H:%M:%S')
+                attributes["Date created"] = datetime.fromtimestamp(os.path.getctime(curr_path)).strftime(
+                    '%Y-%m-%d %H:%M:%S')
+                attributes["n"] = n
+                node = Node(nodeName=x, attributes=attributes)
+                nodes.append(node)
                 if os.path.isfile(curr_path):
-                    attributes={}
-                    attributes["Size"] = os.path.getsize(curr_path)
-                    attributes["Last modified"] = os.path.getmtime(curr_path)
-                    attributes["Date created"] = os.path.getctime(curr_path)
-                    node = Node(nodeName=x, attributes=attributes)
-                    nodes.append(node)
+                    pass
                     # lista.append((x, os.path.getsize(curr_path), filepath.split("\\")[-1]))
                 if os.path.isdir(curr_path):
-                    # lista.append((x, os.path.getsize(curr_path), filepath.split("\\")[-1]))
                     func(curr_path)
         func(path)
 
