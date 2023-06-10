2fc9de7055bb694a2cc4016d11bb76b21f294183
VladimirGolosin <vladimirgolosin1@gmail.com>
2023-06-10 19:58:57 +0200

    line lineee

--- code/Core/core/apps.py
+++ code/Core/core/apps.py
@@ -23,8 +23,6 @@ def load_plugins(group_tag):
     plugins = []
     for ep in pkg_resources.iter_entry_points(group=group_tag):
         p = ep.load()
-        # print("{} {}".format(ep.name, p))
-        # print(group_tag)
         plugin = p()
         plugins.append(plugin)
     return plugins
